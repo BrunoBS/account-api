1f0d55cebb9fbc78d6c7af732630db322df96e2c
Bruno Barbosa da Silva <brunobsgt@gmail.com>
2026-03-21 00:11:17 -0300

    Update PlataformaConfigApplication.java

--- src/main/java/com/brunobs/PlataformaConfigApplication.java
+++ src/main/java/com/brunobs/PlataformaConfigApplication.java
@@ -1,1 +1,12 @@
 
+package com.brunobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaConfigApplication.class, args);
+    }
+}
